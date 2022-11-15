6c4b605982b326c55de2c9fef4da0130d0227144
Luana Batista <luana.b.cruz8@gmail.com>
2022-11-15 18:17:02 -0300

    feat: remove content workload

--- frontend/src/pages/education/parts/education_plan.js
+++ frontend/src/pages/education/parts/education_plan.js
@@ -2,17 +2,14 @@ import Container from 'react-bootstrap/Container';
 import React, { useState } from 'react';
 import ContentWithTitle from '../../../components/ContentWithTitle';
 
-import ContentWorkload from './content_workload';
-
 const EducationPlan = ({data}) => {
 
-  const { program, goals, content_workload, procedure } = data || {};
+  const { program, goals, procedure } = data || {};
 
   return (
     <Container>
       <ContentWithTitle title="Ementa" content={program}/>
       <ContentWithTitle title="Objetivos" content={goals}/>
-      <ContentWorkload data={content_workload}/>
       <ContentWithTitle title="Procedimento de Ensino" content={procedure} />
       <div className='pb-5'></div>
     </Container>
