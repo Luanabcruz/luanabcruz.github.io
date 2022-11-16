19367b74d22d3210581528b6836e28ca0c5d177a
Luana Batista <luana.b.cruz8@gmail.com>
2022-11-15 22:26:04 -0300

    changes

--- frontend/src/pages/education/parts/list_files.js
+++ frontend/src/pages/education/parts/list_files.js
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 import { FilePdfFill } from 'react-bootstrap-icons';
 
 const ListFiles = ({ title, prefix , files }) => {
-
+  
   return (
     <Container>
       <h4>{title}</h4>
@@ -13,7 +13,9 @@ const ListFiles = ({ title, prefix , files }) => {
           <ul>
             {
               files.map((item, index) => (
-                <li><FilePdfFill color="red" /><a href={item?.link} target="_blank" > {prefix} {index+1}</a> - <span style={{fontWeight:'500'}}>{item?.name} </span></li>
+                <li>
+                  <FilePdfFill color="red" /><a href={item?.link} target="_blank" > {prefix} {index+1}</a>  {!!item?.name ? '-' : ''} <span style={{fontWeight:'500'}}>{item?.name} </span>
+                </li>
               )
               )
             }
