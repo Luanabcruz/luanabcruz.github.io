2795158a719a3110320f4dcb2a45ff5fe8b36507
Luana Batista <luana.b.cruz8@gmail.com>
2023-08-03 21:56:16 -0300

    changes

--- frontend/src/pages/lica/LicaPage.js
+++ frontend/src/pages/lica/LicaPage.js
@@ -1,45 +1,49 @@
-const LicaPage = ({ data }) => {
+import React from 'react';
 
+const containerStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+};
+
+const imgStyle = {
+  width: '400px', // Tamanho médio de 400px responsivo
+  maxWidth: '100%', // Para garantir que a imagem não fique maior que o contêiner
+};
+
+const LicaPage = () => {
     const imgUrl = `${process.env.PUBLIC_URL}/logo_lica_no_background.png`
 
-    const containerStyle = {
-        display: 'flex',
-        alignItems: 'top',
-        paddingBottom: '20px',
-        fontSize: '16px'
-    };
-
-    const imgStyle = {
-        width: '250px',
-        marginRight: '10px',
-    };
-
-    return (
-        <div >
-            <div style={containerStyle}>
-                <img src={imgUrl} alt="LICA" className="img-thumbnail" style={imgStyle} />
-                <div style={{textAlign:'justify'}}>
-                    O Laboratório de Inteligência Computacional Aplicada <b>(LICA)</b> é um centro de pesquisa que tem como objetivo aplicar técnicas de inteligência artificial, como aprendizado de máquina, processamento de linguagem natural e visão computacional para solucionar problemas em diferentes áreas, como medicina, finanças, logística e automação industrial. Para realizar as pesquisas e desenvolver soluções inteligentes e personalizadas, o LICA conta com uma equipe formada por alunos e professores dos cursos de ciência da computação e matemática computacional da Universidade Federal do Cariri <b>(UFCA)</b>.
-                </div>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          <div style={containerStyle}>
+            <img src={imgUrl} alt="LICA" style={imgStyle} />
+            
+          </div>
+        </div>
+        <div className="col-md-6">
+        <div style={{ textAlign: 'justify', marginTop: '35px' }}>
+              <p>O Laboratório de Inteligência Computacional Aplicada <b>(LICA)</b> é um centro de pesquisa que tem como objetivo aplicar técnicas de inteligência artificial, como aprendizado de máquina, processamento de linguagem natural e visão computacional para solucionar problemas em diferentes áreas, como medicina, finanças, logística e automação industrial. 
+              </p>
+              <p>
+              Para realizar as pesquisas e desenvolver soluções inteligentes e personalizadas, o LICA conta com uma equipe formada por alunos e professores dos cursos de ciência da computação e matemática computacional da Universidade Federal do Cariri <b>(UFCA)</b>.
+              </p>
             </div>
-            {/* <h4>Discentes</h4>
-            <hr />
-            <ul>
-                <li>Seya de pegasus (seya@gmail.com)</li>
-                <li>Shun de Andromedra (shun@gmail.com)</li>
-                <li>Ikki de Fenix (ikki@gmail.com)</li>
-                <li> Saori Kido (saori@gmail.com)</li>
-            </ul> */}
-
-            <h4>Orientadores</h4>
-            <hr />
-            <ul>
-                <li>Luana Batista da Cruz</li>
-            </ul>
-            <br />
-            <br />
-        </div>);
-}
-
+        </div>
+      </div>
+      <div className='row'>
+        <div className='col-lg-12'>
+          <h4>Orientadores</h4>
+          <hr />
+          <ul>
+            <li>Luana Batista da Cruz</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default LicaPage;
