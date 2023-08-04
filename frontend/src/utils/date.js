467a8f645f04361a3559e125b933d9275df49101
Luana Batista <luana.b.cruz8@gmail.com>
2023-08-03 22:57:56 -0300

    fix date

--- frontend/src/utils/date.js
+++ frontend/src/utils/date.js
@@ -1,18 +1,14 @@
-const formatDate = (dataISO) => {
+function formatDate(dataOriginal) {
+  const dataSemTime = dataOriginal.split('T')[0];
+  const partesData = dataSemTime.split('-');
 
-   
-        const data = new Date(dataISO);
-      
-        if (!(data instanceof Date) || isNaN(data)) {
-          throw new Error('O parâmetro fornecido não é uma data válida.');
-        }
-      
-        const dia = data.getDate().toString().padStart(2, '0');
-        const mes = (data.getMonth() + 1).toString().padStart(2, '0'); // Mês é baseado em zero, por isso adicionamos +1.
-        const ano = data.getFullYear().toString();
-      
-        return `${dia}/${mes}/${ano}`;
+  if (partesData.length !== 3) {
+    return 'Data inválida';
+  }
 
+  const [ano, mes, dia] = partesData;
+
+  return `${dia}/${mes}/${ano}`;
 }
 
 export default formatDate;
