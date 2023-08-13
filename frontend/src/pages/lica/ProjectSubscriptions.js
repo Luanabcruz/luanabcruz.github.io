6211e2dc2dfb57a6c096aa0ee504944487f2d4e9
Luana Batista <luana.b.cruz8@gmail.com>
2023-08-13 08:16:48 -0300

    feat: updaes

--- frontend/src/pages/lica/ProjectSubscriptions.js
+++ frontend/src/pages/lica/ProjectSubscriptions.js
@@ -1,27 +1,46 @@
+import React, { useState, useEffect } from 'react';
 import pageApi from "../../services/page";
 import ProjectList from "./ProjectList";
-import { useState, useEffect } from 'react';
 
-
-const ProjectSubscriptions = ({ data }) => {
-
+const ProjectSubscriptions = () => {
     const [projectsData, setProjectsData] = useState([]);
-    
+    const [openEnrollmentProjects, setOpenEnrollmentProjects] = useState([]);
+    const [closedEnrollmentProjects, setClosedEnrollmentProjects] = useState([]);
+
     useEffect(() => {
         pageApi
         .listSelectionProcesses()
-        .then(response=> setProjectsData(response.data))
+        .then(response => {
+            setProjectsData(response.data);
+        })
         .catch((err) => {
             setProjectsData([]);
-            console.error("ops! ocorreu um erro" + err);
+            console.error("Ops! Ocorreu um erro: " + err);
         });
     }, []);
 
+    useEffect(() => {
+        setOpenEnrollmentProjects(projectsData.filter(project => project.openEnrollment));
+        setClosedEnrollmentProjects(projectsData.filter(project => !project.openEnrollment));
+    }, [projectsData]);
+
     return (
         <>
-            <h2>Inscrições em aberto</h2>
-            <br />
-            <ProjectList projectsData={projectsData}/>
+            {openEnrollmentProjects.length > 0 && (
+                <>
+                    <h2>Inscrições em aberto</h2>
+                    <ProjectList projectsData={openEnrollmentProjects} />
+                </>
+            )}
+            {closedEnrollmentProjects.length > 0 && (
+                <>
+                    <h2>Inscrições encerradas</h2>
+                    <ProjectList projectsData={closedEnrollmentProjects} />
+                </>
+            )}
+            {(openEnrollmentProjects.length === 0 && closedEnrollmentProjects.length === 0) && (
+                <p>Não há inscrições disponíveis no momento.</p>
+            )}
         </>
     );
 }
--- frontend/src/services/page.js
+++ frontend/src/services/page.js
@@ -4,9 +4,10 @@ const api = axios.create({
     baseURL: `${process.env.PUBLIC_URL}/data`,
 });
 
+const isProduction = window.location.hostname === 'api.moonapp.com.br'; // Verifica o domínio de produção
+
 const apiExternal = axios.create({
-    baseURL: `https://api.moonapp.com.br/`,
-    // baseURL: `http://localhost:8000/`,
+    baseURL: isProduction ? 'https://api.moonapp.com.br/' : 'http://localhost:8000/',
 });
 
 const pageApi = {
